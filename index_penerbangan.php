64dcfc41fc5f41e4be8d9b9a63cf837f6d72a456
Ari <setiawan.ari28@gmail.com>
2021-02-12 22:55:32 +0700

    CRUD penerbangan dan penumpang

--- index_penerbangan.php
+++ index_penerbangan.php
@@ -2,18 +2,16 @@
     <head>
         <title>Daftar Penerbangan</title>
         <style>
-            /* MENAMPILKAN BORDER PADA TABLE */
             table,td,tr,th { border: 1px solid black}
         </style>
     </head>
     <body>
         <?php
-            //MENDEFINISIKAN KONEKSI DATABASE
             $username = "root";
             $password = "";
             $server_name = "localhost";
             $database_name = "penerbangan"; 
-        
+
             $connection = new mysqli($server_name, $username, $password, $database_name);
             if($connection->connect_error)
                 echo "Error, konfigurasi DB salah";
@@ -26,7 +24,7 @@
             }
         ?>
         <b>Daftar Penerbangan</b>
-        <a href="insert_penerbangan.php">Tambah Penerbangan</a>
+        <a href="insert_penerbangan.php">Tambah penerbangan</a>
         <a href="index.php">Daftar Bandara</a>
 
         <form action="" method="GET">
@@ -36,8 +34,8 @@
 
         <table>
             <tr>
-                <td>Nomor</td>
-                <td>ID Pesawat</td>
+                <td>No</td>
+                <td>Pesawat</td>
                 <td>Bandara Asal</td>
                 <td>Bandara Tujuan</td>
                 <td>Waktu Penerbangan</td>
@@ -47,35 +45,77 @@
                 <td>Delete</td>
             </tr>
             <?php
-                $sql_penerbangan = "SELECT * FROM penerbangan";
+                $sql_penerbangan = "SELECT 
+                    penerbangan.id,
+                    penerbangan.waktu_penerbangan,
+                    penerbangan.status_penerbangan, 
+                    pesawat.kode_pesawat,
+                    pesawat.nama_pesawat,
+                    bandaraAsal.nama_bandara as bandara_asal,
+                    bandaraTujuan.nama_bandara as bandara_tujuan
+                    FROM penerbangan, 
+                    bandara as bandaraAsal,  bandara as bandaraTujuan,
+                    pesawat 
+                    WHERE 
+                        penerbangan.id_pesawat=pesawat.id AND 
+                        penerbangan.id_bandara_dari=bandaraAsal.id AND 
+                        penerbangan.id_bandara_tujuan=bandaraTujuan.id 
+                    ORDER BY penerbangan.id DESC";
                 if($keyword!=""){
-                    $sql_penerbangan = "SELECT * FROM penerbangan WHERE 
-                        status_penerbangan LIKE '%".$keyword."%' OR 
-                        waktu_penerbangan LIKE '%".$keyword."%'";
+                    $sql_penerbangan = "SELECT 
+                    penerbangan.id,
+                    penerbangan.waktu_penerbangan,
+                    penerbangan.status_penerbangan, 
+                    pesawat.kode_pesawat,
+                    pesawat.nama_pesawat,
+                    bandaraAsal.nama_bandara as bandara_asal,
+                    bandaraTujuan.nama_bandara as bandara_tujuan
+                    FROM penerbangan, 
+                    bandara as bandaraAsal,  bandara as bandaraTujuan,
+                    pesawat 
+                    WHERE 
+                        penerbangan.id_pesawat=pesawat.id AND 
+                        penerbangan.id_bandara_dari=bandaraAsal.id AND 
+                        penerbangan.id_bandara_tujuan=bandaraTujuan.id AND 
+                        (
+                            bandaraAsal.nama_bandara LIKE '%".$keyword."%' OR 
+                            bandaraTujuan.nama_bandara LIKE '%".$keyword."%' OR 
+                            pesawat.nama_pesawat LIKE '%".$keyword."%'
+                        )
+                        ORDER BY penerbangan.id DESC";
+                    //keyword='bandara'
+
                 }
 
                 $result_penerbangan = $connection->query($sql_penerbangan);
-                //MENGECEK APAKAH HASIL DATANYA ADA
                 if($result_penerbangan->num_rows>0){
                     $i = 1;
                     //PERULANGAN UNTUK MENGAMBIL DATA HASIL QUERY
                     while($row = $result_penerbangan->fetch_assoc()){
                         echo "<tr>";
                         echo "<td>".$i."</td>";
-                        echo "<td>".$row['id_pesawat']."</td>";
-                        echo "<td>".$row['id_bandara_dari']."</td>";
-                        echo "<td>".$row['id_bandara_tujuan']."</td>";
-                        echo "<td>".$row['waktu_penerbangan']."</td>";
-                        echo "<td>".$row['status_penerbangan']."</td>";
-                        echo "<td><a href='update_customer.php?id=".$row['id']."'>Update</a></td>";
-                        echo "<td><a href='delete_customer.php?id=".$row['id']."'>Delete</a></td>";
+                        echo "<td>".$row['kode_pesawat'].' - '.$row['nama_pesawat']."</td>";
+                        echo "<td>".$row['bandara_asal']."</td>";
+                        echo "<td>".$row['bandara_tujuan']."</td>";
+                        echo "<td>".date('d M Y h:i', strtotime($row['waktu_penerbangan']))."</td>";
+                        if($row['status_penerbangan']==1)
+                            echo "<td>Belum Terbang</td>";
+                        else if($row['status_penerbangan']==2)
+                            echo "<td>Menunggu Penumpang</td>";
+                        else if($row['status_penerbangan']=3)
+                            echo "<td>Sudah Terbang</td>";
+                        else if($row['status_penerbangan']==4)
+                            echo "<td>Sudah Sampai</td>";
+                        echo "<td><a href='detail_penerbangan.php?id=".$row['id']."'>Detail</a></td>";
+                        echo "<td><a href='update_penerbangan.php?id=".$row['id']."'>Update</a></td>";
+                        echo "<td><a href='delete_penerbangan.php?id=".$row['id']."'>Delete</a></td>";
                         echo "</tr>";
                         $i++;
                     }
                 }else{
-                    echo "<tr><td colspan='7'>Tidak ada data yang ditampilkan</td></tr>";
+                    echo "<tr><td colspan='9'>Tidak ada data yang ditampilkan</td></tr>";
                 }
             ?>
         </table>
     </body>
-</html>
+</html> 
--- insert_penerbangan.php
+++ insert_penerbangan.php
@@ -1,41 +1,80 @@
 <html>
     <head>
-        <title>Insert PENERBANGAN</title>
+        <title>Insert Penerbangan</title>
     </head>
     <body>
         <b>Form Penerbangan</b><br/>
-        <form method="POST" action="">
-            Pesawat: <input type="text" name="id_pesawat"><br/>
-            Bandara Asal: <input type="text" name="id_bandara_dari"><br/>
-            Bandara Tujuan: <input type="text" name="id_bandara_tujuan"><br/>
-            Nama Pesawat: <input type="text" name="nama_pesawat"><br/>
-            Nama Maskapai: <input type="text" name="nama_maskapai"><br/>
+        <?php
+            $username = "root";
+            $password = "";
+            $server_name = "localhost";
+            $database_name = "penerbangan"; 
+
+            $connection = new mysqli($server_name, $username, $password, $database_name);
+
+            $sql_pesawat = "SELECT * FROM pesawat";
+            $result_pesawat = $connection->query($sql_pesawat);
+
+            $sql_bandara = "SELECT * FROM bandara";
+            $result_bandara = $connection->query($sql_bandara);
+            $result_bandara_tujuan = $connection->query($sql_bandara);
+        ?>
+        <form method="POST" action="" onsubmit="return periksaAsalTujuan()">
+            Pesawat: 
+            <select name="id_pesawat">
+                <?php
+                while($row = $result_pesawat->fetch_assoc()){
+                    echo "<option value=".$row['id'].">".$row['nama_pesawat']."</option>";
+                }
+                ?>
+            </select>
+            <br/>
+            Bandara Asal: 
+            <select name="id_bandara_dari" id="bandara_dari" onchange="periksaAsalTujuan()">
+            <?php
+                while($row = $result_bandara->fetch_assoc()){
+                    echo "<option value=".$row['id'].">".$row['nama_bandara']."</option>";
+                }
+                ?>
+            </select>
+            <br/>
+            Bandara Tujuan: 
+            <select name="id_bandara_tujuan" id="bandara_tujuan" onchange="periksaAsalTujuan()">
+            <?php
+                while($row = $result_bandara_tujuan->fetch_assoc()){
+                    echo "<option value=".$row['id'].">".$row['nama_bandara']."</option>";
+                }
+                ?>
+            </select>
+            <br/>
+            Waktu Penerbangan: <input type="date" name="tanggal_penerbangan"><input type="time" name="jam_penerbangan"><br/>
+            Status: 
+            <select name="status_penerbangan">
+                <option value="1">Belum Terbang</option>
+                <option value="2">Menunggu Penumpang</option>
+                <option value="3">Sudah Terbang</option>
+                <option value="4">Sudah Sampai</option>
+            </select><br/>
             <button type="submit">SIMPAN</button>
         </form>
         <?php
-            //JIKA ADA VARIBEL POST, MAKA JALANKAN KODE INI
-            //VARIABEL POST DIDAPATKAN DARI PROSES KETIKA SIMPAN DATA
-            //ATAU KLIK SUBMIT
             if(count($_POST)>1){
-                $username = "root";
-                $password = "";
-                $server_name = "localhost";
-                $database_name = "penerbangan"; 
-            
-                $connection = new mysqli($server_name, $username, $password, $database_name);
                 if(!$connection->connect_error){
-                    $sql_bandara = "INSERT INTO pesawat (kode_pesawat, 
-                        tahun_pembuatan, nama_pesawat, nama_maskapai, 
+                    $gabung_tanggal = $_POST['tanggal_penerbangan'].' '.$_POST['jam_penerbangan'];
+                    $sql_penerbangan = "INSERT INTO penerbangan (id_pesawat, 
+                        id_bandara_dari, id_bandara_tujuan, 
+                        waktu_penerbangan, status_penerbangan, 
                         created_at, updated_at) VALUE(
-                        '".$_POST['kode_pesawat']."',
-                        '".$_POST['tahun_pembuatan']."', 
-                        '".$_POST['nama_pesawat']."', 
-                        '".$_POST['nama_maskapai']."', 
+                        '".$_POST['id_pesawat']."',
+                        '".$_POST['id_bandara_dari']."', 
+                        '".$_POST['id_bandara_tujuan']."', 
+                        '".$gabung_tanggal."',
+                        '".$_POST['status_penerbangan']."', 
                         '".date('Y-m-d h:i:s')."', 
                         '".date('Y-m-d h:i:s')."')";
 
-                    if($connection->query($sql_bandara) === TRUE){
-                        header('Location: index_pesawat.php');
+                    if($connection->query($sql_penerbangan) === TRUE){
+                        header('Location: index_penerbangan.php');
                     }
                     else{
                         echo "Data gagal dimasukkan:".$connection->error;
@@ -43,5 +82,18 @@
                 }
             }
         ?>
+    <script>
+        function periksaAsalTujuan(){
+            var bandara_dari = document.getElementById('bandara_dari').value
+            var bandara_tujuan = document.getElementById('bandara_tujuan').value
+            if(bandara_dari==bandara_tujuan){
+                alert("Pilihan Bandara Tidak Boleh Sama");
+                return false
+            }
+            else{
+                return true
+            }
+        }
+    </script>
     </body>
-</html>
+</html> 
